42f84c142adf0a5862b6213640c333b4c4c1be78
Anna Sakhaudinova <104850911+atsa21@users.noreply.github.com>
2025-06-30 13:45:24 +0300

    feat: add-host-firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAGJgodawLzwHUE8UcY5AL0bBpmnHxyfO8",
+    authDomain: "ddashboardd-32356.firebaseapp.com",
+    projectId: "ddashboardd-32356",
+    storageBucket: "ddashboardd-32356.firebasestorage.app",
+    messagingSenderId: "669625659822",
+    appId: "1:669625659822:web:914293ce653dfb1f2a983f"
+  }
+};
