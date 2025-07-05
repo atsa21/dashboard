ac09bc6d5f31f0ab4f890307767518067f626087
Anna Sakhaudinova <104850911+atsa21@users.noreply.github.com>
2025-07-05 18:23:08 +0300

    feat: add-images

--- src/app/core/pipes/money.pipe.ts
+++ src/app/core/pipes/money.pipe.ts
@@ -4,16 +4,20 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'money'
 })
 export class MoneyPipe implements PipeTransform {
-  transform(value: number): string {
+  transform(value: number, format = 'short'): string {
     if (typeof value !== 'number') {
       return '';
     }
-    if (value >= 1_000_000) {
-      return `$${(value / 1_000_000).toFixed(1)}M`;
+    
+    if (format === 'sort') {
+      if (value >= 1_000_000) {
+        return `$${(value / 1_000_000).toFixed(1)}M`;
+      }
+      if (value >= 100_000) {
+        return `$${Math.round(value / 1_000)}K`;
+      }
     }
-    if (value >= 100_000) {
-      return `$${Math.round(value / 1_000)}K`;
-    }
-    return value.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+  
+    return value.toLocaleString('en-US', { style: 'currency', currency: 'USD', minimumFractionDigits: 0 });
   }
 }
